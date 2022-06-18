e7c6c2003e902602f5d029896e125937d523265b
Codehouzem <codehouseinc@gmail.com>
2022-06-18 12:11:13 +0100

    fix:web pack issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-    // loader: "cloudinary",
-    // path: ["https://res.cloudinary.com/dfsmwu4av/image/upload/v1648649470"],
-  // },
+  
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
