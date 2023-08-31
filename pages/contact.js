d5e1dd0816ff4b2e097a2768d6980f25ee8eb0c9
Codehouze <chijioke.emma.maxwell@gmail.com>
2023-08-31 20:36:59 +0200

    scafold backend fileStructure

--- pages/contact.js
+++ pages/contact.js
@@ -5,14 +5,32 @@ import Button from "@mui/material/Button";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 import React from "react";
 import Link from "next/link";
+import { Formik } from "formik";
 // import Image from "next/image";
 import Breadcrumb from "../Components/BreadCrumb";
 
 function contact() {
+  const initialValue = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  };
+
+  const formValidation = {};
+
+  const handleSubmit = (values, helpers) => {
+    console.log(values);
+    // make sure values is not an empty value
+    // make axios call to send the values to the backend.
+    // reset form after wards
+    helpers.resetForm();
+  };
+
   return (
     <>
       <Grid container sx={{ px: 3, mt: 5 }}>
-        <Grid item xs={12} sm={6} lg={6} sx={{ p: 5, fontFamily: "Arial" }}>
+        <Grid item xs={12} sm={6} lg={6} sx={{ p: 10 }}>
           <Typography variant="h3" component="h3">
             Contact Us
           </Typography>
@@ -49,70 +67,97 @@ function contact() {
           </Grid>
         </Grid>
         <Grid item direction="column" xs={12} sm={6} lg={6} className="contact">
-          <Grid
-            item
-            alignitems="center"
-            justify="center"
-            className="contactCard"
-          >
-            <Grid item>
-              <TextField
-                id="filled-basic"
-                label="First Name"
-                variant="outlined"
-                className="formInput"
-                style={{ width: "50%" }}
-                sx={{ mt: 2 }}
-              />
-            </Grid>
+          <Formik initialValues={initialValue} onSubmit={handleSubmit}>
+            {(props) => (
+              <form onSubmit={props.handleSubmit}>
+                <Grid
+                  item
+                  alignitems="center"
+                  justify="center"
+                  className="contactCard"
+                >
+                  <Grid item>
+                    <TextField
+                      id="filled-basic"
+                      label="First Name"
+                      variant="outlined"
+                      className="formInput"
+                      style={{ width: "50%" }}
+                      sx={{ mt: 2 }}
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="firstName"
+                    />
+                  </Grid>
 
-            <Grid item>
-              <TextField
-                id="filled-basic"
-                label="Last Name"
-                variant="outlined"
-                className="formInput"
-                style={{ width: "50%" }}
-                sx={{ mt: 2 }}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="filled-basic"
-                label="Email"
-                variant="outlined"
-                style={{ width: "50%" }}
-                sx={{ mt: 2 }}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                label="Phone Number"
-                id="margin-none"
-                variant="outlined"
-                style={{ width: "50%" }}
-                sx={{ mt: 2 }}
-              />
-            </Grid>
-            <Grid item>
-              <TextareaAutosize
-                aria-label="maximum height"
-                minRows={9}
-                placeholder="Minimum 3 rows"
-                style={{ width: "50%" }}
-                sx={{ mt: 2 }}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                variant="contained"
-                style={{ width: "40%" }}
-                sx={{ mt: 2, mr: "auto" }}
-              >
-                Send Message
-              </Button>
-            </Grid>
-          </Grid>
+                  <Grid item>
+                    <TextField
+                      id="filled-basic"
+                      label="Last Name"
+                      variant="outlined"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="lastName"
+                      className="formInput"
+                      style={{ width: "50%" }}
+                      sx={{ mt: 2 }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      id="filled-basic"
+                      label="Email"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="email"
+                      variant="outlined"
+                      style={{ width: "50%" }}
+                      sx={{ mt: 2 }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      label="Phone Number"
+                      id="margin-none"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="email"
+                      variant="outlined"
+                      style={{ width: "50%" }}
+                      sx={{ mt: 2 }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextareaAutosize
+                      aria-label="maximum height"
+                      minRows={9}
+                      placeholder="Minimum 3 rows"
+                      style={{ width: "50%" }}
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="message"
+                      sx={{ mt: 2 }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      style={{ width: "40%" }}
+                      sx={{ mt: 2, mr: "auto" }}
+                      type="submit"
+                    >
+                      Send Message
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          </Formik>
         </Grid>
       </Grid>
     </>
